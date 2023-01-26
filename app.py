959be550d727d7b3aea7dd3c36ef864798179c6c
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2023-01-26 14:50:54 +0000

    Added a base.html file to act as the template for each page. index.html acts as my home page/starting page. form.html is the first form that will need to be filled out. style.css is the css style guide that I have added into the base.html mostly this was just to add a margain for the p tags.

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", content="Testing")
 
-@app.route("/form")
+
+@app.route("/form1")
 def form():
     return render_template("form.html")
+
 
 @app.route("/data/", methods = ['POST', "GET"])
 def data():
@@ -17,7 +19,7 @@ def data():
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == "POST":
         form_data = request.form
-        return render_template("data.html", form_data = form_data)
+        return render_template("data.html", form_data=form_data)
 
 
 if __name__ == "__main__":
